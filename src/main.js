1d2441edfdf8871b1b4d6866b9f287d0e55a657c
thanhthien0706 <thanhthien24102002@gmail.com>
2022-01-17 14:48:20 +0700

    setup file thanh thien

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from "./store";
+import { BootstrapVue } from "bootstrap-vue";
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// swipper
+import 'swiper/css/swiper.css'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(VueAwesomeSwiper)
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="main_app" class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+      return {}
+    },
+};
+</script>
+
+<style>
+</style>
