67218f38c9d2720fc6b8a00fa2d10e8ff649b7df
Phong Vo <vosonphong16@gmail.com>
2022-01-24 21:58:41 +0700

    24-01

---
+++ .history/src/pages/Admin/user_20220124214802.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user"></div>
+</template>
+<script>
+    
+export default {
+  name: "admin_user",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ .history/src/pages/Admin_20220124214743.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-wrapper main-wrapper-2">
+       
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Admin",
+  data() {
+    return {};
+  },
+};
+
+</script>
+
+<style>
+</style>
